afc407fa56270afd4aed40cc703668a63794e67d
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-07-05 15:55:00 -0500

    Add basic opengl framework

--- ConnectFour.cpp
+++ ConnectFour.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
-class ConnectFour{
+class ConnectFour {
     int board[6][7] = {};
     int turn = 1;
     int count = 0;
 
-    public:
+public:
 
-    int getTurn(){
+    int getTurn() {
         return turn;
     }
 
-    int getCount(){
+    int getCount() {
         return count;
     }
 
-    int get(int row, int column){
+    int get(int row, int column) {
         return board[row][column];
     }
 
 
     //Returns 1 or 2 if a player won, a 0 if game is still going, and a -1 for a stalemate
-    int won(){
+    int won() {
 
         //Check for enough tokens
-        if(count < 8){
+        if (count < 8) {
             return 0;
         }
         //Check for stalemate
-        if(count == 42){
+        if (count == 42) {
             return -1;
         }
 
@@ -37,60 +38,60 @@ class ConnectFour{
         int color = 0;
 
         //Check horizontal connections
-        for(int row = 0; row < 6; row++){
-            for(int column = 0; column < 2; column++){
-                if(board[row][column]){
+        for (int row = 0; row < 6; row++) {
+            for (int column = 0; column < 2; column++) {
+                if (board[row][column]) {
                     color = board[row][column];
                     sum = board[row][column];
-                    for(int i = 1; i < 4; i++){
+                    for (int i = 1; i < 4; i++) {
                         sum += board[row][column + i];
                     }
-                    if(sum == color * 4){
+                    if (sum == color * 4) {
                         return color;
                     }
                 }
             }
         }
         //Check vertical connections
-        for(int row = 0; row < 3; row++){
-            for(int column = 0; column < 7; column++){
-                if(board[row][column]){
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 7; column++) {
+                if (board[row][column]) {
                     color = board[row][column];
                     sum = board[row][column];
-                    for(int i = 1; i < 4; i++){
+                    for (int i = 1; i < 4; i++) {
                         sum += board[row + i][column];
                     }
-                    if(sum == color * 4){
+                    if (sum == color * 4) {
                         return color;
                     }
                 }
             }
         }
         //Check for downward diagonal connections
-        for(int row = 0; row < 3; row++){
-            for(int column = 0; column < 4; column++){
-                if(board[row][column]){
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 4; column++) {
+                if (board[row][column]) {
                     color = board[row][column];
                     sum = board[row][column];
-                    for(int i = 1; i < 4; i++){
+                    for (int i = 1; i < 4; i++) {
                         sum += board[row + i][column + i];
                     }
-                    if(sum == color * 4){
+                    if (sum == color * 4) {
                         return color;
                     }
                 }
             }
         }
         //Check for upward diagonal connections
-        for(int row = 0; row < 3; row++){
-            for(int column = 3; column < 7; column--){
-                if(board[row][column]){
+        for (int row = 0; row < 3; row++) {
+            for (int column = 3; column < 7; column--) {
+                if (board[row][column]) {
                     color = board[row][column];
                     sum = board[row][column];
-                    for(int i = 1; i < 4; i++){
+                    for (int i = 1; i < 4; i++) {
                         sum += board[row + i][column - i];
                     }
-                    if(sum == color * 4){
+                    if (sum == color * 4) {
                         return color;
                     }
                 }
@@ -100,11 +101,11 @@ class ConnectFour{
         return 0;
     }
 
-    int drop(int column){
+    int drop(int column) {
         int row = 5;
-        while(board[row][column]){
+        while (board[row][column]) {
             row--;
-            if(row < 0){
+            if (row < 0) {
                 return 0;
             }
         }
@@ -114,6 +115,37 @@ class ConnectFour{
     }
 };
 
-int main(){
-  return 0;
+int main() {
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
