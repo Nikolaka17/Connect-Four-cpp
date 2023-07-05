139ad396ddd9ed1d24268ebec83dc023666a05d1
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-07-05 14:05:58 -0500

    Add base connect four functionality

--- ConnectFour.cpp
+++ ConnectFour.cpp
@@ -1,3 +1,119 @@
-int main(){
+#include <iostream>
 
+class ConnectFour{
+    int board[6][7] = {};
+    int turn = 1;
+    int count = 0;
+
+    public:
+
+    int getTurn(){
+        return turn;
+    }
+
+    int getCount(){
+        return count;
+    }
+
+    int get(int row, int column){
+        return board[row][column];
+    }
+
+
+    //Returns 1 or 2 if a player won, a 0 if game is still going, and a -1 for a stalemate
+    int won(){
+
+        //Check for enough tokens
+        if(count < 8){
+            return 0;
+        }
+        //Check for stalemate
+        if(count == 42){
+            return -1;
+        }
+
+        //Variables for continuity checking
+        int sum = 0;
+        int color = 0;
+
+        //Check horizontal connections
+        for(int row = 0; row < 6; row++){
+            for(int column = 0; column < 2; column++){
+                if(board[row][column]){
+                    color = board[row][column];
+                    sum = board[row][column];
+                    for(int i = 1; i < 4; i++){
+                        sum += board[row][column + i];
+                    }
+                    if(sum == color * 4){
+                        return color;
+                    }
+                }
+            }
+        }
+        //Check vertical connections
+        for(int row = 0; row < 3; row++){
+            for(int column = 0; column < 7; column++){
+                if(board[row][column]){
+                    color = board[row][column];
+                    sum = board[row][column];
+                    for(int i = 1; i < 4; i++){
+                        sum += board[row + i][column];
+                    }
+                    if(sum == color * 4){
+                        return color;
+                    }
+                }
+            }
+        }
+        //Check for downward diagonal connections
+        for(int row = 0; row < 3; row++){
+            for(int column = 0; column < 4; column++){
+                if(board[row][column]){
+                    color = board[row][column];
+                    sum = board[row][column];
+                    for(int i = 1; i < 4; i++){
+                        sum += board[row + i][column + i];
+                    }
+                    if(sum == color * 4){
+                        return color;
+                    }
+                }
+            }
+        }
+        //Check for upward diagonal connections
+        for(int row = 0; row < 3; row++){
+            for(int column = 3; column < 7; column--){
+                if(board[row][column]){
+                    color = board[row][column];
+                    sum = board[row][column];
+                    for(int i = 1; i < 4; i++){
+                        sum += board[row + i][column - i];
+                    }
+                    if(sum == color * 4){
+                        return color;
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    int drop(int column){
+        int row = 5;
+        while(board[row][column]){
+            row--;
+            if(row < 0){
+                return 0;
+            }
+        }
+        board[row][column] = turn;
+        turn = 3 - turn;
+        return won();
+    }
+};
+
+int main(){
+  return 0;
 }
