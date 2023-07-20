0944032421dc0bb71603912ea74ad73b2c110bae
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-07-19 19:16:28 -0500

    Add graphics and two player playability

--- ConnectFour.cpp
+++ ConnectFour.cpp
@@ -1,92 +1,13 @@
 #include <iostream>
 #include <climits>
+#include "raylib.h"
 
 class ConnectFour {
     int board[6][7];
-    int turn = 1;
+    int turn;
     int count = 0;
     bool done = false;
     int recent = 0;
-
-    //Returns 1 or 2 if a player won, a 0 if game is still going, and a -1 for a stalemate
-    int won() {
-
-        //Check for enough tokens
-        if (count < 8) {
-            return 0;
-        }
-        //Check for stalemate
-        if (count == 42) {
-            return -1;
-        }
-
-        //Variables for continuity checking
-        int sum = 0;
-        int color = 0;
-
-        //Check horizontal connections
-        for (int row = 0; row < 6; row++) {
-            for (int column = 0; column < 2; column++) {
-                if (board[row][column]) {
-                    color = board[row][column];
-                    sum = board[row][column];
-                    for (int i = 1; i < 4; i++) {
-                        sum += board[row][column + i];
-                    }
-                    if (sum == color * 4) {
-                        return color;
-                    }
-                }
-            }
-        }
-        //Check vertical connections
-        for (int row = 0; row < 3; row++) {
-            for (int column = 0; column < 7; column++) {
-                if (board[row][column]) {
-                    color = board[row][column];
-                    sum = board[row][column];
-                    for (int i = 1; i < 4; i++) {
-                        sum += board[row + i][column];
-                    }
-                    if (sum == color * 4) {
-                        return color;
-                    }
-                }
-            }
-        }
-        //Check for downward diagonal connections
-        for (int row = 0; row < 3; row++) {
-            for (int column = 0; column < 4; column++) {
-                if (board[row][column]) {
-                    color = board[row][column];
-                    sum = board[row][column];
-                    for (int i = 1; i < 4; i++) {
-                        sum += board[row + i][column + i];
-                    }
-                    if (sum == color * 4) {
-                        return color;
-                    }
-                }
-            }
-        }
-        //Check for upward diagonal connections
-        for (int row = 0; row < 3; row++) {
-            for (int column = 3; column < 7; column++) {
-                if (board[row][column]) {
-                    color = board[row][column];
-                    sum = board[row][column];
-                    for (int i = 1; i < 4; i++) {
-                        sum += board[row + i][column - i];
-                    }
-                    if (sum == color * 4) {
-                        return color;
-                    }
-                }
-            }
-        }
-
-        return 0;
-    }
 
     //Give a numerical evaluation for game state, negative for player 1, positive for player 2 (AI)
     int eval() {
@@ -162,12 +83,14 @@ class ConnectFour {
     }
 
 public:
-    ConnectFour() {
+    ConnectFour(int startTurn) {
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
                 board[i][j] = 0;
             }
         }
+
+        turn = startTurn;
     }
 
     ConnectFour(const ConnectFour& old) {
@@ -176,6 +99,8 @@ public:
                 board[i][j] = old.board[i][j];
             }
         }
+        turn = old.turn;
+        count = old.count;
     }
     ConnectFour& operator=(const ConnectFour& old);
 
@@ -191,6 +116,10 @@ public:
         return board[row][column];
     }
 
+    bool isDone() {
+        return done;
+    }
+
     //Place a token in a column, return a boolean based off if the move went through
     bool drop(int column) {
         if (done) {
@@ -199,7 +128,7 @@ public:
         int row = 5;
         while (board[row][column - 1]) {
             row--;
-            if (row < 0) {
+            if (row <= 0) {
                 recent = 0;
                 return false;
             }
@@ -207,7 +136,7 @@ public:
         board[row][column - 1] = turn;
         count++;
         turn = 3 - turn;
-        done = won() != 0;
+        done = (won() != 0);
         recent = column;
         return true;
     }
@@ -231,8 +160,141 @@ public:
         }
         return bestMove;
     }
+
+    //Returns 1 or 2 if a player won, a 0 if game is still going, and a -1 for a stalemate
+    int won() {
+
+        //Check for enough tokens
+        if (count < 8) {
+            return 0;
+        }
+        //Check for stalemate
+        if (count == 42) {
+            return -1;
+        }
+
+        //Variables for continuity checking
+        bool continuity = true;
+        int color = 0;
+
+        //Check horizontal connections
+        for (int row = 0; row < 6; row++) {
+            for (int column = 0; column < 2; column++) {
+                if (board[row][column]) {
+                    color = board[row][column];
+                    continuity = true;
+                    for (int i = 1; i < 4; i++) {
+                        continuity = continuity && (board[row][column + i] == color);
+                    }
+                    if (continuity) {
+                        return color;
+                    }
+                }
+            }
+        }
+        //Check vertical connections
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 7; column++) {
+                if (board[row][column]) {
+                    color = board[row][column];
+                    continuity = true;
+                    for (int i = 1; i < 4; i++) {
+                        continuity = continuity && (board[row + i][column] == color);
+                    }
+                    if (continuity) {
+                        return color;
+                    }
+                }
+            }
+        }
+        //Check for downward diagonal connections
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 4; column++) {
+                if (board[row][column]) {
+                    color = board[row][column];
+                    continuity = true;
+                    for (int i = 1; i < 4; i++) {
+                        continuity = continuity && (board[row + i][column + i] == color);
+                    }
+                    if (continuity) {
+                        return color;
+                    }
+                }
+            }
+        }
+        //Check for upward diagonal connections
+        for (int row = 0; row < 3; row++) {
+            for (int column = 3; column < 7; column++) {
+                if (board[row][column]) {
+                    color = board[row][column];
+                    continuity = true;
+                    for (int i = 1; i < 4; i++) {
+                        continuity = continuity && (board[row + i][column - i] == color);
+                    }
+                    if (continuity) {
+                        return color;
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
 };
 
 int main() {
+    //Graphics constants
+    const int WIDTH = 800; //Width of screen
+    const int HEIGHT = 500; //Height of screen
+    const int RADIUS = 30; //Radius of token
+    const int VOFFSET = 15; //Vertical offset
+    const int HOFFSET = 25; //Horrizontal offset
+
+    //Game constants
+    const int MAX_DEPTH = 5;
+    
+    //Graphics setup
+    InitWindow(WIDTH, HEIGHT, "Connect Four");
+    SetTargetFPS(60);
+
+    //Game setup
+    ConnectFour game = ConnectFour(1);
+    bool running = true;
+    bool AiTurn = false;
+
+    //Event loop
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        //Draw components
+        ClearBackground(BLUE);
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                int color = game.get(i, j);
+                DrawCircle(((WIDTH / 7) * j) + RADIUS + HOFFSET, ((HEIGHT / 6) * i) + RADIUS + VOFFSET, RADIUS, (color == 1)?RED:((color == 2)?YELLOW:WHITE));
+            }
+        }
+        
+        //Action listeners
+        if (running && IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !AiTurn) {
+            int column = GetMouseX() / (WIDTH / 7);
+            if (column != 7) {
+                column++;
+            }
+            if (game.drop(column)) {
+                //AiTurn = true;
+                running = !game.isDone();
+            }
+        }
+        /*else if (AiTurn) {
+            game.drop(game.AiMove(game, std::min(MAX_DEPTH, 42 - game.getCount())));
+            AiTurn = false;
+            running = (game.won() == 0);
+        }*/
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
